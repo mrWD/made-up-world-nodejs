80e45f444d3bf68508bdffd539aa93b1845b1227
viktor <lvigtor@gmail.com>
2020-05-11 22:04:41 +0300

    Update cors.

--- src/config/connectCors.ts
+++ src/config/connectCors.ts
@@ -1,12 +1,12 @@
 import cors from 'cors';
 
 const whitelist = [
-  'http://localhost:5000/',
-  'http://192.168.0.48:5000/',
-  'http://localhost:8080/',
-  'http://192.168.0.48:8080/',
-  'https://made-up-world-vuejs.herokuapp.com/',
-  'https://made-up-world-nodejs.herokuapp.com/',
+  'http://localhost:5000',
+  'http://192.168.0.48:5000',
+  'http://localhost:8080',
+  'http://192.168.0.48:8080',
+  'https://made-up-world-vuejs.herokuapp.com',
+  'https://made-up-world-nodejs.herokuapp.com',
 ];
 const corsOptions = {
   origin: (origin: any, callback: any) => {
